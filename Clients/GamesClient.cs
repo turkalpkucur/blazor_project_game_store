87d93b753b0bba211e9b9bf5ef5c4038eb79c2bd
Türkalp Kucur <turkalp.kucur@gmail.com>
2025-08-21 16:14:10 +0300

    güncelleme
    güncelleme

--- Clients/GamesClient.cs
+++ Clients/GamesClient.cs
@@ -27,5 +27,23 @@ ReleaseDate=new DateOnly(2022,9,27)
 },
 ];
 
+    private readonly Genre[] genres = new GenresClient().GetGenres();
+
+
     public GameSummary[] GetGames() => [.. games];
+
+    public void AddGame(GameDetails game)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
+        var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+        var GameSummary = new GameSummary
+        {
+            Id = games.Count + 1,
+            Name = game.Name,
+            Genre = genre.Name,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+        games.Add(GameSummary);
+    }
 }
