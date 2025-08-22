6909c8d7901882a71f78e536c012a105d49213f0
Türkalp Kucur <turkalp.kucur@gmail.com>
2025-08-22 11:33:44 +0300

    güncelleme
    güncelleme

--- Clients/GamesClient.cs
+++ Clients/GamesClient.cs
@@ -13,14 +13,14 @@ Price=1.99M,
 ReleaseDate=new DateOnly(1992,7,15)
 },
 new (){
-Id=1,Name="Final Fantasy XIV",
+Id=2,Name="Final Fantasy XIV",
 Genre="Roleplaying",
 Price=59.99M,
 ReleaseDate=new DateOnly(2010,9,30)
 },
 
 new (){
-Id=1,Name="FIFA 2023",
+Id=3,Name="FIFA 2023",
 Genre="Sports",
 Price=69.99M,
 ReleaseDate=new DateOnly(2022,9,27)
--- Models/GameDetails.cs
+++ Models/GameDetails.cs
@@ -1,12 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameStore.Frontend.Models;
 
 public class GameDetails
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+    [Required]
     public string? GenreId { get; set; }
+    [Range(1, 100)]
     public decimal Price { get; set; }
     public DateOnly ReleaseDate { get; set; }
 }
